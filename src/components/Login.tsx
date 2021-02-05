6ef098a02a7684b6c0aa89cd43a303c84e04136d
TreaTeT <treatett@centrum.sk>
2021-02-05 22:00:35 +0100

    added react-hook-form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  name: string;
+  password: string;
+}
+
 function Register() {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = ({ name, password }: IForm) => {
+    console.log(`user: ${name} , password: ${password}`);
+  };
+
   return (
     <div className="min-h-screen min-w-full bg-gradient-to-b from-blue-700 to-blue-500 flex items-center">
       <div className="bg-white flex-col sm:w-6/12 lg:w-3/12 mx-auto min-h-2/4 shadow-2xl rounded-sm">
         <div className="m-5">
           <Link
-            className="outline-none  font-roboto text-xl text-blue-600 font-semibold"
+            className="outline-none font-roboto text-xl text-blue-600 font-semibold"
             to="/"
           >
             {"🡸"}
@@ -18,7 +31,7 @@ function Register() {
           </p>
         </div>
 
-        <form className="w-full">
+        <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
           {/* USERNAME */}
           <div className="w-full">
             <p className="w-3/4  tracking-wide  mx-auto text-blue-500 leading-relaxed font-trocchi text-lg mt-10">
@@ -29,20 +42,32 @@ function Register() {
               className="w-3/4 rounded-b mx-auto pl-3 py-2 font-semibold text-blue-600 placeholder-blue-400 outline-none flex my-5 border-t-2 shadow-md border-blue-500 border-opacity-80 "
               name="name"
               placeholder="username"
+              ref={register({ required: true, minLength: 5 })}
             />
+            {errors.name && (
+              <div className="text-red-500 font-sans font-semibold w-3/4 mx-auto text-xs text-opacity-70">
+                Username must at least 5 characters long!
+              </div>
+            )}
           </div>
 
           {/* PASSWORD */}
           <div className="w-full">
-            <p className="w-3/4 mx-auto  tracking-widetext-blue-500 leading-relaxed font-trocchi text-blue-500 text-lg  my-5">
+            <p className="w-3/4 mx-auto  tracking-widetext-blue-500 leading-relaxed font-trocchi text-blue-500 text-lg my-5">
               password
             </p>
             <input
               type="password"
-              className="w-3/4 rounded-b shadow-md mx-auto pl-3 py-2  mb-10 font-semibold text-blue-600 placeholder-blue-400  outline-none flex border-t-2 border-blue-500 border-opacity-80"
+              className="w-3/4 rounded-b shadow-md mx-auto pl-3 py-2  mb-5 font-semibold text-blue-600 placeholder-blue-400  outline-none flex border-t-2 border-blue-500 border-opacity-80"
               name="password"
               placeholder="verystrongsecurepasswordplease"
+              ref={register({ required: true, minLength: 8 })}
             />
+            {errors.password && (
+              <div className="text-red-500 font-sans font-semibold w-3/4 mx-auto text-xs text-opacity-70">
+                That's not a valid password
+              </div>
+            )}
           </div>
 
           <button
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  name: string;
+  email: string;
+  password: string;
+}
 function Register() {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = ({ name, email, password }: IForm) => {
+    console.log(`user: ${name}
+    email: ${email}
+    password: ${password}`);
+  };
+
   return (
     <div className="min-h-screen min-w-full bg-gradient-to-b from-blue-700 to-blue-500 flex items-center">
       <div className="bg-white flex-col sm:w-6/12 lg:w-3/12 mx-auto min-h-2/4 shadow-2xl rounded-sm ">
@@ -17,7 +32,7 @@ function Register() {
             Registration
           </p>
         </div>
-        <form className="w-full">
+        <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
           {/* USERNAME */}
           <div className="w-full">
             <p className="w-3/4  tracking-wide  mx-auto text-blue-500 leading-relaxed font-trocchi text-lg  mt-10">
@@ -25,35 +40,53 @@ function Register() {
             </p>
             <input
               type="username"
-              className="w-3/4 rounded-b mx-auto pl-3 py-2 font-semibold text-blue-600 placeholder-blue-400 outline-none flex my-5 border-t-2 shadow-md border-blue-500 border-opacity-80 "
+              className="w-3/4 rounded-b mx-auto pl-3 py-2 font-semibold text-blue-600 placeholder-blue-400 outline-none flex mt-5 mb-2 border-t-2 shadow-md border-blue-500 border-opacity-80 "
               name="name"
               placeholder="username"
+              ref={register({ required: true, minLength: 4 })}
             />
+            {errors.name && (
+              <div className="text-red-500 font-sans font-semibold w-3/4 mx-auto text-xs text-opacity-70">
+                Username must at least 5 characters long!
+              </div>
+            )}
           </div>
 
           {/* EMAIL */}
           <div className="w-full">
-            <p className="w-3/4 tracking-wide mx-auto text-blue-500 text-underline leading-relaxed font-trocchi text-lg">
+            <p className="w-3/4 tracking-wide mx-auto text-blue-500 text-underline leading-relaxed font-trocchi text-lg mt-4">
               e-mail
             </p>
             <input
               type="email"
-              className="w-3/4 rounded-b mx-auto pl-3 py-2  font-semibold text-blue-600 placeholder-blue-400 outline-none flex my-5 border-t-2 shadow-md border-blue-500 border-opacity-80"
+              className="w-3/4 rounded-b mx-auto pl-3 py-2  font-semibold text-blue-600 placeholder-blue-400 outline-none flex mb-2 mt-5 border-t-2 shadow-md border-blue-500 border-opacity-80"
               name="email"
               placeholder="jozko@mrkvicka.com"
+              ref={register({ required: true })}
             />
+            {errors.email && (
+              <div className=" text-red-500 font-sans font-semibold text-xs text-opacity-70">
+                Enter a valid title!
+              </div>
+            )}
           </div>
           {/* PASSWORD */}
-          <div className="w-full">
+          <div className="w-full ">
             <p className="w-3/4 mx-auto  tracking-widetext-blue-500 leading-relaxed font-trocchi text-blue-500 text-lg  my-5">
               password
             </p>
             <input
               type="password"
-              className="w-3/4 rounded-b shadow-md mx-auto pl-3 py-2  mb-10 font-semibold text-blue-600 placeholder-blue-400  outline-none flex border-t-2 border-blue-500 border-opacity-80"
+              className="w-3/4 rounded-b shadow-md mx-auto pl-3 py-2  mb-2 font-semibold text-blue-600 placeholder-blue-400  outline-none flex border-t-2 border-blue-500 border-opacity-80"
               name="password"
               placeholder="verystrongsecurepasswordplease"
+              ref={register({ required: true, minLength: 8 })}
             />
+            {errors.password && (
+              <div className=" w-3/4 mx-auto text-red-500 font-sans font-semibold text-xs text-opacity-70">
+                Enter a valid title!
+              </div>
+            )}
           </div>
 
           <button
