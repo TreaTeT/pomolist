37d3d69647fb6712a03181a2e1db582054dc6660
TreaTeT <treatett@centrum.sk>
2021-03-02 19:55:28 +0100

    heroku deploy alpha

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth.header";
 
-const API_URL = "http://localhost:5000/api/users/";
+const API_URL = "https://pomolist.herokuapp.com/";
 
 class UserService {
   getUserTasks(id) {
