055c94e1a666d1bec1ca965c8452a8685d18b385
TreaTeT <treatett@centrum.sk>
2021-03-04 17:19:43 +0100

    fix n7

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth.header";
 
-const API_URL = "https://pomolist.herokuapp.com/api/users/";
+const API_URL = "/api/users/";
 
 class UserService {
   getUserTasks(id) {
